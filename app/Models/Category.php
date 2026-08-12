fd96944aa93cb55f8eabc1d94b9e28a2ae799ab7
ROSEJC <trhaidong2005@gmail.com>
2026-08-13 00:48:18 +0700

    feat/#98900-update-product-schema

---
+++ app/Models/Category.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['parent_id', 'name', 'slug', 'description'];
+
+    // Lấy danh mục cha
+    public function parent()
+    {
+        return $this->belongsTo(Category::class, 'parent_id');
+    }
+
+    // Lấy các danh mục con
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+}
